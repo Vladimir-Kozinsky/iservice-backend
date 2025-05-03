780d4eeef263bdb4ef45f19af92c72b5f7e74c36
Uladzimir Kazinski <kozinskytxc@gmail.com>
2025-05-03 20:47:41 +0400

    feat: add port env

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document)
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(5000);
+  //await app.listen(5000);
+  await app.listen(process.env.PORT, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
