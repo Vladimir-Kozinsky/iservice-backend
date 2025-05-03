31777541a8a2fe3f3c35cbe554de5bed0aae0589
Uladzimir Kazinski <kozinskytxc@gmail.com>
2025-05-03 22:22:24 +0400

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: ['content-type', 'Access-Control-Allow-Origin'],
-    origin: ['https://vladimir-kozinsky.github.io/iservice'],
+    // allowedHeaders: ['content-type', 'Access-Control-Allow-Origin'],
+    origin: ['https://vladimir-kozinsky.github.io/iservice/'],
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
   })
   // somewhere in your initialization file
   app.use(cookieParser());
