967575fdd20c318230c2cd16867cd2b1c46c1eea
Uladzimir Kazinski <kozinskytxc@gmail.com>
2025-06-05 12:59:25 +0400

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     //allowedHeaders: ['origin', 'x-requested-with', 'content-type', 'accept', 'authorization', 'Access-Control-Allow-Origin', '*'],
-    origin: ['https://vladimir-kozinsky.github.io', 'http://localhost:3000'],
+    origin: ['https://vladimir-kozinsky.github.io', 'http://localhost:3000', 'https://vladimir-kozinsky.github.io'],
     credentials: true,
     //methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
   })
