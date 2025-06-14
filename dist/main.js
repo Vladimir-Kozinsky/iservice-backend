105b68febcd612911906bc03e10f12c76149aa0e
Uladzimir Kazinski <kozinskytxc@gmail.com>
2025-06-14 11:58:37 +0400

    fix cors

--- dist/main.js
+++ dist/main.js
@@ -8,10 +8,10 @@ const cookieParser = require("cookie-parser");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        allowedHeaders: 'Content-Type, Authorization, Accept, Origin, X-Auth-Token, Content-Length, X-JSON, Observe',
-        origin: ['https://vladimir-kozinsky.github.io', 'https://vladimir-kozinsky.github.io/', 'https://vladimir-kozinsky.github.io/*', 'https://www.vladimir-kozinsky.github.io/*', 'https://www.vladimir-kozinsky.github.io', 'http://localhost:3000'],
+        allowedHeaders: ['origin', 'x-requested-with', 'content-type', 'accept', 'authorization', 'Access-Control-Allow-Origin', '*'],
+        origin: ['https://vladimir-kozinsky.github.io'],
         credentials: true,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS, UPDATE",
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
     });
     app.use(cookieParser());
     const config = new swagger_1.DocumentBuilder()
@@ -23,7 +23,7 @@ async function bootstrap() {
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api/docs', app, document);
     app.useGlobalPipes(new common_1.ValidationPipe());
-    await app.listen(5000);
+    await app.listen(process.env.PORT, '0.0.0.0');
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
